c49b9262e9efa84871f9ed1077f8118a749e37d0
Hamed Zaghaghi <hamed.zaghaghi@gmail.com>
2018-09-27 23:50:17 +0330

    Add query for answer

--- index.py
+++ index.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
+
+@app.route("/query", methods=["POST"])
+def query():
+    data = request.json
+    response = {
+        'answer': data.get('question', '') + '?'
+    }
+    return json.dumps(response)
--- static/js/index.js
+++ static/js/index.js
@@ -2,14 +2,6 @@
   
   var chat = {
     messageToSend: '',
-    messageResponses: [
-      'Why did the web developer leave the restaurant? Because of the table layout.',
-      'How do you comfort a JavaScript bug? You console it.',
-      'An SQL query enters a bar, approaches two tables and asks: "May I join you?"',
-      'What is the most used language in programming? Profanity.',
-      'What is the object-oriented way to become wealthy? Inheritance.',
-      'An SEO expert walks into a bar, bars, pub, tavern, public house, Irish pub, drinks, beer, alcohol'
-    ],
     init: function() {
       this.cacheDOM();
       this.bindEvents();
@@ -39,19 +31,26 @@
         this.$textarea.val('');
         
         // responses
-        var templateResponse = Handlebars.compile( $("#message-response-template").html());
-        var contextResponse = { 
-          response: this.getRandomItem(this.messageResponses),
-          time: this.getCurrentTime()
-        };
-        
-        setTimeout(function() {
-          this.$chatHistoryList.append(templateResponse(contextResponse));
-          this.scrollToBottom();
-        }.bind(this), 1500);
-        
+        let instance = this;
+        $.ajax({
+          url: '/query',
+          method: 'post',
+          contentType: 'application/json; charset=UTF-8',
+          data: JSON.stringify({
+            recipient: 'wikipedia-en',
+            question: this.messageToSend.trim()
+          }),
+          dataType: 'json'
+        }).done(function(resp) {
+          var templateResponse = Handlebars.compile($("#message-response-template").html());
+          var contextResponse = {
+            response: resp.answer,
+            time: instance.getCurrentTime()
+          };
+          instance.$chatHistoryList.append(templateResponse(contextResponse));
+          instance.scrollToBottom();
+        });
       }
-      
     },
     
     addMessage: function() {
